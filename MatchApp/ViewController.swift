5e993758fe0ed7929f38eb05097927003f98ab6d
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-08-05 03:49:08 +1000

    Protocols and delegates initiation.

--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     let model = CardModel()
     var cardsArray = [Card]()
 
