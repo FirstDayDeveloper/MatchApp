390d849a2b0ad4061588ff93c7b024d0570062e5
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-08-05 23:08:29 +1000

    Protocols and delegates implementation in the View Controller

--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -7,9 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -21,8 +19,32 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
        cardsArray = model.getCards()
+        
+        // set the view controller as the data source and the delegate of the collectionview
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
     }
+// MARK: - Collection View Delegate Methods
 
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // Return number of cards by pulling from the cards array
+        return cardsArray.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        // TODO: Finish configuring the cell
+        
+        // return it
+        
+        return cell
+    }
 }
 
