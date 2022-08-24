bc8c5da8ba55bbb6e469bb0b7d5ccc01f852dc3d
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-08-24 15:26:30 +0300

    Adding the card flipping logic

--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -38,13 +38,37 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         // get a cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
         
-        // TODO: Finish configuring the cell
+        // Get the card from the card array
+        
+        let card = cardsArray[indexPath.row]
+        
+        // Finish configuring the cell
+        cell.configureCell(card: card)
         
         // return it
         
         return cell
     }
+    
+    // Handling user tap events
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Get a reference to the cell that was tapped
+        let cell = collectionView.cellForItem(at: indexPath) as? CardCollectionViewCell
+        
+        // Check the status of the card to determine how to flip it
+        if cell?.card?.isFlipped == false {
+            // Flip the card up
+            cell?.flipUp()
+        }
+        else {
+            // Flip the card down
+            cell?.flipDown()
+        }
+       
+    }
+    
 }
 
